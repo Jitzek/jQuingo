598384dab31ea7d714a9548d5f64ade36f2a21c8
Jitzek <jitz_k@hotmail.com>
2021-12-07 19:56:59 +0100

    Undo unintentional merge

--- src/jquingo/router/route.ts
+++
@@ -1,9 +0,0 @@
-import { jQuingoComponent } from "../component/component";
-import { router } from "./router";
-
-export class jQuingoRoute extends jQuingoComponent {
-  constructor(public path: string) {
-    super();
-    router.addRoute(this);
-  }
-}
