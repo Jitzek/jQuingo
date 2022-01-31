a4d013bd27ad1859e026b5d979b9918d719049a9
Jitzek <jitz_k@hotmail.com>
2022-01-31 18:46:58 +0100

    Redo of f/jjk/component-styling

--- webpack.development.js
+++ webpack.development.js
@@ -1,6 +1,10 @@
 const path = require("path");
+const common = require("./webpack.config.js");
+const { merge } = require("webpack-merge");
 
-module.exports = {
+process.env.NODE_ENV = "development";
+
+module.exports = merge(common, {
   // https://webpack.js.org/guides/development/#using-source-maps
   // TLDR: Maps compiled code back to the original source code to easily track where an error occured
   devtool: "inline-source-map",
@@ -9,4 +13,4 @@ module.exports = {
     port: 8080,
     liveReload: true,
   }
-};
+});
